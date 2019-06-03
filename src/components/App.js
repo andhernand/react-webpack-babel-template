86c074b91e8c05a0ccb6f412b47920f2489690ee
Andres Hernandez <andhernand@gmail.com>
2019-06-02 19:49:15 -0600

    adding styled-components and styled-theming

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
-import './App.scss';
+import styled, { css, ThemeProvider } from 'styled-components';
+import theme from 'styled-theming';
 
-const App = () => <div>React Webpack Babel Template</div>;
+const white = '#fff';
+const black = '#000';
+
+const boxStyles = theme('mode', {
+  light: css`
+    background: ${white};
+    color: ${black};
+  `,
+  dark: css`
+    background: ${black};
+    color: ${white};
+  `
+});
+
+const StyledDiv = styled.div`
+  ${boxStyles}
+`;
+
+const App = () => {
+  return (
+    <ThemeProvider theme={{ mode: 'dark' }}>
+      <StyledDiv>React Webpack Babel Template</StyledDiv>
+    </ThemeProvider>
+  );
+};
 
 export default App;
