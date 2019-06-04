e57769d8209e71bf3170ae9a06a92016e191da4b
Andres Hernandez <andhernand@gmail.com>
2019-06-03 21:04:34 -0600

    properly configure babel/preset-env
    also remove webpack optimization (until I understand it better).

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+  entry: [path.resolve(__dirname, 'src', 'index.js')],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].[hash:9].js'
@@ -79,28 +79,5 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'build'),
     hot: true
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(
-              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-            )[1];
-
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          }
-        }
-      }
-    }
   }
 };
