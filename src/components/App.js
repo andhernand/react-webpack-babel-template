b7f614f7564f075ccc665ffe888d117ad7406a18
Andres Hernandez <andhernand@gmail.com>
2019-06-23 07:46:47 -0600

    removing es-components (too much bloat).

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import viaTheme from 'es-components-via-theme';
-import { Heading } from 'es-components';
 
 function App() {
   return (
-    <ThemeProvider theme={viaTheme}>
-      <Heading level={1} isKnockoutStyle>
+      <h1>
         React Webpack Babel Template
-      </Heading>
-    </ThemeProvider>
+      </h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { StyleReset } from 'es-components';
 
-function ResetApp() {
-  return (
-    <React.Fragment>
-      <StyleReset />
-      <App />
-    </React.Fragment>
-  );
-}
-
-ReactDOM.render(<ResetApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot) {
   module.hot.accept();
