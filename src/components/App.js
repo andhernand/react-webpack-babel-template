b1bc6671af29e48785082f4daf21e2bc7f6d0dfa
Andres Hernandez <andres.hernandez@extendhealth.com>
2019-07-19 15:32:09 -0600

    adding back es-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { StyleReset, Heading } from 'es-components';
+import { ThemeProvider } from 'styled-components';
+import viaTheme from 'es-components-via-theme';
 
 function App() {
   return (
-      <h1>
-        React Webpack Babel Template
-      </h1>
+    <ThemeProvider theme={viaTheme}>
+      <React.Fragment>
+        <StyleReset />
+        <Heading isKnockoutStyle level={1}>
+          React Webpack Babel Template
+        </Heading>
+      </React.Fragment>
+    </ThemeProvider>
   );
 }
 
