a102d16b649c7dedd34ebd2c3bedc5f56defaa85
Andres Hernandez <andhernand@gmail.com>
2019-07-20 16:42:07 -0600

    updating the base application layout.
    bumped two dependencies

--- src/components/About/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Heading } from 'es-components';
-
-const About = () => {
-  return <Heading level={2}>Welcome About</Heading>;
-};
-
-export default About;
--- src/components/Home/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Heading } from 'es-components';
-
-const Home = () => {
-  return <Heading level={2}>Welcome Home</Heading>;
-};
-
-export default Home;
