e56e6bfee233abb7a1ebf09f6fb00fe4482544d6
Andres Hernandez <andhernand@gmail.com>
2020-06-21 08:20:07 -0600

    adjusting accessibility issues found with axe.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const App = () => {
-  return <span>Your App Goes Here!</span>;
+  return (
+    <>
+      <header role="banner">
+        <h1>Header</h1>
+      </header>
+      <nav role="navigation">
+        <ul>
+          <li>Home</li>
+        </ul>
+      </nav>
+      <main role="main">
+        <p>main.</p>
+      </main>
+      <footer role="contentinfo">
+        <p>footer.</p>
+      </footer>
+    </>
+  );
 };
 
 export default App;
