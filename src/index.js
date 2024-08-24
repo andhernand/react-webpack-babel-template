10c4459b833a4f1f98a2e140c6d6b89de6b72f07
Andres Hernandez <andhernand@gmail.com>
2024-08-23 19:15:59 -0600

    updating a whole lot of stuff

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import App from "./components/App";
+import App from "./components/App.js";
 import "./style.css";
 
 const rootElement = document.getElementById("app");
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
